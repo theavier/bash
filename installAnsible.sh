e5295dc8c050d494e5d9525a88d54957acc4174e
theavier <theavier@gmail.com>
2020-01-22 12:54:58 +0100

    Update installAnsible.sh

--- installAnsible.sh
+++ installAnsible.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 # Usage Example:
-# ./install_ansible.sh 'v1.1' path/to/local/ansible/hosts/file
-# Look up https://github.com/ansible/ansible for version tags to use
-
-versionTag=$1
-ansibleHosts=$2
+# ./install_ansible.sh 
 
 # Install Ansible Dependencies
 echo '[Install Ansible] Install dependencies'
@@ -15,3 +11,4 @@ sudo apt-get install -y python-setuptools
 echo '[Install Ansible] '
 sudo ansible
 
+echo '[Install Ansible] Done!'

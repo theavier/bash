1a689d370ddc446d49791402357af7224aa8822d
theavier <theavier@gmail.com>
2020-02-26 16:49:05 +0100

    Update installAnsible.sh

--- installAnsible.sh
+++ installAnsible.sh
@@ -9,6 +9,7 @@ sudo apt-get install -y python-setuptools
 
 # Install Ansible
 echo '[Install Ansible] '
+sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible 
 
 echo '[Install Ansible] Done!'

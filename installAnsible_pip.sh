fcb7bee3b1462486d5fd4bcf18e6283f4dce158d
theavier <theavier@gmail.com>
2022-12-27 15:52:23 +0100

    Update installAnsible_pip.sh

--- installAnsible_pip.sh
+++ installAnsible_pip.sh
@@ -3,15 +3,6 @@
 # Usage Example:
 # ./install_ansible.sh 
 
-# Install Ansible Dependencies
-echo '[Install Ansible] Install dependencies'
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt-get install -y python3-pip python3.11 #python-setuptools
-# set default python
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
-sudo update-alternatives --config python3
-
 # Install Ansible
 echo '[Install Ansible] '
 python3 -m pip install --upgrade --user pip

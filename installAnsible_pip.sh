e5485ca57dddc4af3dfb30f7b9290149c602bf06
theavier <theavier@gmail.com>
2022-12-27 11:20:16 +0100

    Update installAnsible_pip.sh

--- installAnsible_pip.sh
+++ installAnsible_pip.sh
@@ -5,7 +5,7 @@
 
 # Install Ansible Dependencies
 echo '[Install Ansible] Install dependencies'
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get install -y python3-pip python3.11 #python-setuptools
 
 # Install Ansible

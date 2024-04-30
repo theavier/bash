5ece8e8fad07f3f7ab4982674ede6b7adaf13f9f
theavier <theavier@gmail.com>
2024-04-30 18:57:17 +0000

    Update CoreApps.sh

--- CoreApps.sh
+++ CoreApps.sh
@@ -1,7 +1,13 @@
 apt-get install git
 
+snap install spotify
 # install onlyoffice
-wget http://download.onlyoffice.com/docs/docs-install.sh
-bash docs-install.sh
+#wget http://download.onlyoffice.com/docs/docs-install.sh
+#bash docs-install.sh
 
-
+# install onedrive
+wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_22.04/ ./" | sudo tee /etc/apt/sources.list.d/onedrive.list
+apt-get update
+apt install --no-install-recommends --no-install-suggests onedrive
+    

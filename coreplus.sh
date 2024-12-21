08eec78cd96b47298b9e2a58c38d7a43f1ca9d81
theavier <theavier@gmail.com>
2024-12-21 10:44:19 +0100

    Update coreplus.sh

--- coreplus.sh
+++ coreplus.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 apt-get install snapd 
 snap install pycharm-community --classic --edge
+cp -v /snap/pycharm-community/current/meta/gui/pycharm-community.desktop ~/.local/share/applications
